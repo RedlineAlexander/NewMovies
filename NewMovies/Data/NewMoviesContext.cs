4444e1f8a2e00b4035677268af5bd3a70d092168
RedlineAlexander <redlinerailmetro@gmail.com>
2020-06-30 19:24:23 +0300

    Moviess

---
+++ NewMovies/Data/NewMoviesContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewMovies.Models;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewMovies.Data
+{
+    public class NewMoviesContext : DbContext
+    {
+        public NewMoviesContext(DbContextOptions<NewMoviesContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Movie> Movie { get; set; }
+
+    /////    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+     //  {
+//
+    //    }
+
+    }
+}
